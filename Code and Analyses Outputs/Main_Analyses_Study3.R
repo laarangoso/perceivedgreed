f77d2742f06f57fc7681af5e10fc33bead2cd4c3
unknown <81339006+laarangoso@users.noreply.github.com>
2022-02-02 22:21:03 +1100

    _

--- Code and Analyses Outputs/Main_Analyses_Study3.R
+++ Code and Analyses Outputs/Main_Analyses_Study3.R
@@ -172,7 +172,7 @@ anova(ranked_model3)
 
 #e. Plot
 
-ggplot(anc_data2, aes(Group, Greed))+
+ggplot(anc_data3, aes(Group, Greed))+
   geom_boxplot(aes(fill=Group)) + labs (y="Greed Score") + theme_bw() + 
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
   scale_fill_brewer(palette = "Pastel1") + ylim(4,7)
